78f024281794bdfebcbf4e5fda6b21822b0579c8
Timothy Carter <talic2002@gmail.com>
2022-01-26 20:32:01 -0500

    Added objects with question and answer choices

---
+++ play.js
@@ -0,0 +1,40 @@
+// objects containing questions and question choices
+
+let questions = [
+    {question: 'What is the hardest substance in the human body?',
+    choice1: 'alpha-keratin',
+    choice2: 'bone',
+    choice3: 'cartilage',
+    choice4: 'muscle tissue',
+    choice5: 'tooth enamel',
+    choice6: 'erythrocytes',
+    answer: 5
+    },
+    {question: 'How many different organ systems make up the human body?',
+    choice1: 'eleven',
+    choice2: 'twenty',
+    choice3: 'three',
+    choice4: 'seven',
+    choice5: 'fifteen',
+    choice6: 'one',
+    answer: 1
+    },
+    {question: 'What is the most common element in the known universe?',
+    choice1: 'carbon',
+    choice2: 'plutonium',
+    choice3: 'lithium',
+    choice4: 'hydrogen',
+    choice5: 'helium',
+    choice6: 'nitrogen',
+    answer: 4
+    },
+    {question: 'What is the largest (thickest) layer of Earth\'s atmosphere?',
+    choice1: 'troposphere',
+    choice2: 'asthenosphere',
+    choice3: 'exosphere',
+    choice4: 'stratosphere',
+    choice5: 'mesosphere',
+    choice6: 'ionosphere',
+    answer: 3
+    },
+    ]
